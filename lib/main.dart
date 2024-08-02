97ac7f0e7d79b1de0cbe4ba98b623679f11ef8dd
Dibs07 <banerjeedibs@gmail.com>
2024-08-02 23:34:19 +0530

    modified:   lib/main.dart 	modified:   lib/screens/home_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/components/background.dart';
+import 'package:frontend/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: BackgroundDesign(),
+      home: HomePage(),
     );
   }
 }
