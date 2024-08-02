e3ffde9dd93d1b0b9bef986ce1ec810de46ed8de
sayan <sayanadh123@gmail.com>
2024-08-02 22:33:36 +0530

    new file:   lib/constants/colors.dart

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    surface: Colors.white,
+    primary: Color(0xFF2F80ED),
+    secondary: Color(0xFF5DC6FF),
+    tertiary: Color(0xFFB9E6FF),
+  )
+);
